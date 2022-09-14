9c35fc484dc91cc4b408de4019e69ec7cf34e4b3
engineerriazmahmud <engineerriazmahmud@gmail.com>
2022-09-14 18:30:03 +0600

    end

--- fahrenheit_to_celsius.c
+++ fahrenheit_to_celsius.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main () {
-  printf("Hello World");
-  return 0;
+int main (void) {
+    float fahrenheit, celsius;
+
+    /* Input temperature in fahrenheit */
+    printf("Enter Temperature in Fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    /* Fahrenheit to celsius conversion formula */
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    /* Print the value of celsius */
+    printf("%.2f Fahrenheit = %.2f Celsius", fahrenheit, celsius);
+
+return 0;
 }
